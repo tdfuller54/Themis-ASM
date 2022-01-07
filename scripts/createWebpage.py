d9caa2fd001b08689062a56a14a32e3ca0c8e81e
tdfuller54 <tdfuller54@gmail.com>
2022-01-06 20:56:13 -0600

    changed inner table width back to 50 for test

--- scripts/createWebpage.py
+++ scripts/createWebpage.py
@@ -129,7 +129,7 @@ TABLE = """
             border-width: 2px 6px 12px 18px;
         }
         table {
-            width: 100%;
+            width: 50%;
             border: 1px solid #000000;
             border-collapse: collapse;
         }
