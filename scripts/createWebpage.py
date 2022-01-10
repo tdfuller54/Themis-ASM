dd5d88bfd828fa32dd8569a31ca600ec96be55b0
tdfuller54 <tdfuller54@gmail.com>
2022-01-10 14:48:40 -0600

    horizontal break try 2

--- scripts/createWebpage.py
+++ scripts/createWebpage.py
@@ -323,10 +323,12 @@ def indexMd(fastas, assemblies, combos, finalfolder):
                     '<tr style="border: none;background: transparent;">',
                     '<td style="border: none;background: transparent;"> ' + testHtml(tablines[0]) + ' </td>',
                     '</tr>',
-                    '</table>'])
+                    '</table>',
+                    ''])
 
 
-    mdlines.extend(['<hr>',
+    mdlines.extend(['---',
+                    '',
                     '<a name="asmqual"></a>',
                     '## Assembly Quality Comparisons',
                     #f'<embed src="{finalfolder}/combined_ngx_plot.pdf" type="application/pdf" width="100%" height="600 px" />',
