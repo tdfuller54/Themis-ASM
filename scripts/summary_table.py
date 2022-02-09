50701a53262387c22592e73ae096f4f5f8b3fb03
tdfuller54 <tdfuller54@gmail.com>
2022-02-08 22:01:35 -0600

    change to float

--- scripts/summary_table.py
+++ scripts/summary_table.py
@@ -62,7 +62,7 @@ for i in snakemake.input["gapstats"]:
         l = gsts.readline()
         s = l.rstrip().split()
         solid["nGaps"].append(s[0])
-        solid["%nGaps"].append("{:.4f}".format(int(float(s[1]))))
+        solid["%nGaps"].append("{:.4f}".format(float(s[1])))
 
 # Populate merqury entries
 for i in snakemake.input["merqv"]:
